64cec096645356e6e1a889eec3108c8a504ca174
Christof Flück <christof.flueck@comfone.com>
2022-12-02 10:45:38 +0100

    Day 1

--- day01.py
+++ day01.py
@@ -2,18 +2,53 @@ from run_util import run_puzzle
 
 
 def part_a(data):
-    input = [int(x) for x in data.split(',')]
-    return 0
+    input = [0 if x == '' else int(x) for x in data.split('\n')]
+    elfs = get_elfs(input)
+    return max(elfs)
+
+
+def get_elfs(input):
+    curr = 0
+    elfs = []
+    for i in input:
+        if i == 0:
+            elfs.append(curr)
+            curr = 0
+        else:
+            curr += i
+    if curr > 0:
+        elfs.append(curr)
+    return elfs
 
 
 def part_b(data):
-    input = [int(x) for x in data.split(',')]
-    return 0
+    input = [0 if x == '' else int(x) for x in data.split('\n')]
+    elfs = get_elfs(input)
+    top3 = 0
+    for _ in range(3):
+        top = max(elfs)
+        top3 += top
+        print(top)
+        elfs.remove(top)
+    return top3
 
 
 def main():
     examples = [
-        ("""""", 1, 1)
+        ("""1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000""", 24000, 45000)
     ]
     day = int(__file__.split('/')[-1].split('.')[0][-2:])
     run_puzzle(day, part_a, part_b, examples)
