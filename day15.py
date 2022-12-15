7d914baaf0e750c5d227833d22bdeea04fe0a7d3
christofflueck <christof.flueck@gmail.com>
2022-12-15 23:30:19 +0100

    Day 15 - Reeeeeeaaaaaaaalllyyyy slow. But works

--- day15.py
+++ day15.py
@@ -9,33 +9,68 @@ def part_a(data):
 
     target_row = 10 if pairs[0][0][0] == 2 else 2000000
 
-    area = {}
+    area = set()
+    blocked = set()
 
     for sensor, beacon in pairs:
         sensor_x, sensor_y = sensor
         beacon_x, beacon_y = beacon
 
-        diff_x = abs(sensor_x - beacon_x)
-        diff_y = abs(sensor_y - beacon_y)
+        distance = get_distance(beacon, sensor)
+        if sensor_y == target_row:
+            blocked.add(sensor)
+        if beacon_y == target_row:
+            blocked.add(beacon)
 
-        distance = diff_x + diff_y
-        area[sensor] = 0
-        area[beacon] = 0
-        for distance_y in range(-distance, distance + 1):
-            remaining_distance = distance - abs(distance_y)
-            for distance_x in range(-remaining_distance, remaining_distance + 1):
-                point_x = sensor_x + distance_x
-                point_y = sensor_y + distance_y
-                point = (point_x, point_y)
-                if point not in area:
-                    area[point] = 1
+        target_diff_y = target_row - sensor_y
+        if abs(target_diff_y) <= distance:
+            max_x = distance - abs(target_diff_y)
+            for x in range(-max_x, max_x + 1):
+                area.add((sensor_x + x, target_row))
 
-    return sum([area[p] for p in area.keys() if p[1] == target_row])
+        print('pair')
+
+    return len([x for x in area if x not in blocked])
+
+
+def get_distance(beacon, sensor):
+    sensor_x, sensor_y = sensor
+    beacon_x, beacon_y = beacon
+    diff_x = abs(sensor_x - beacon_x)
+    diff_y = abs(sensor_y - beacon_y)
+    distance = diff_x + diff_y
+    return distance
+
+
+def safe_add_possible(possible, x, y, max_coord):
+    if max_coord >= x >= 0 and max_coord >= y >= 0:
+        possible.add((x, y))
 
 
 def part_b(data):
-    input = [int(x) for x in data.split(',')]
-    return 0
+    input = [parse('Sensor at x={:d}, y={:d}: closest beacon is at x={:d}, y={:d}', x) for x in data.split('\n')]
+    pairs = [((sx, sy), (bx, by)) for sx, sy, bx, by in input]
+    max_coord = 20 if pairs[0][0][0] == 2 else 4000000
+
+    possible = set()
+    for sensor, beacon in pairs:
+        distance = get_distance(beacon, sensor) + 1
+        sensor_x, sensor_y = sensor
+
+        safe_add_possible(possible, sensor_x - distance, sensor_y, max_coord)
+        safe_add_possible(possible, sensor_x + distance, sensor_y, max_coord)
+
+        possible = set([b for b in possible if get_distance(b, sensor) > distance - 1])
+        for diff_x in range(-distance, distance + 1):
+            safe_add_possible(possible, sensor_x + diff_x, sensor_y - (distance - abs(diff_x)), max_coord)
+            safe_add_possible(possible, sensor_x + diff_x, sensor_y + (distance - abs(diff_x)), max_coord)
+
+    for sensor, beacon in pairs:
+        distance = get_distance(beacon, sensor)
+        possible = set([b for b in possible if get_distance(b, sensor) > distance])
+
+    x, y = possible.pop()
+    return x * 4000000 + y
 
 
 def main():
@@ -53,7 +88,7 @@ Sensor at x=20, y=14: closest beacon is at x=25, y=17
 Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3""", 26, 1)
+Sensor at x=20, y=1: closest beacon is at x=15, y=3""", 26, 56000011)
     ]
     day = int(__file__.split('/')[-1].split('.')[0][-2:])
     run_puzzle(day, part_a, part_b, examples)
